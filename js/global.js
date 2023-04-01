a994e649a0cfe34bb837fd32aa1a73f7685a62b3
Leksa <admin@petiteweb.ru>
2023-04-01 15:03:41 +0300

    setLike

---
+++ js/global.js
@@ -0,0 +1,8 @@
+const block = document.querySelector(".wrapper");
+let name = "lk_band";
+let path = `https://cats.petiteweb.dev/api/single/${name}`;
+
+/*
+    JSON.stringify(obj) => преобразует объект в строку
+    JSON.parse(str) => преобразует строку в объект
+*/
